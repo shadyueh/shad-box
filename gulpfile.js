615dbe1360948240b6ae7b67dcf558b421cdf453
Held Grijo <shadyueh@gmail.com>
2018-01-20 10:57:47 -0400

    Including browser-sync and gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+});
+
+gulp.task('bs', function() {
+    browserSync.init({
+        proxy: "localhost/shad-box"
+    });
+});
